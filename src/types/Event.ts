13aa61519cfa31793538266a13cb7e9a62a82167
Michael Pretorius <michaelpretorius96@gmail.com>
2024-11-28 09:16:34 +0200

    feat: add new events and update ixo sdk

--- src/types/Event.ts
+++ src/types/Event.ts
@@ -5,16 +5,26 @@ export enum EventTypes {
   // entity
   createEntity = "ixo.entity.v1beta1.EntityCreatedEvent",
   updateEntity = "ixo.entity.v1beta1.EntityUpdatedEvent",
+  verifiedEntity = "ixo.entity.v1beta1.EntityVerifiedUpdatedEvent",
+  transferEntity = "ixo.entity.v1beta1.EntityTransferredEvent",
+  accountCreatedEntity = "ixo.entity.v1beta1.EntityAccountCreatedEvent",
   // claims
   createCollection = "ixo.claims.v1beta1.CollectionCreatedEvent",
   updateCollection = "ixo.claims.v1beta1.CollectionUpdatedEvent",
   submitClaim = "ixo.claims.v1beta1.ClaimSubmittedEvent",
   updateClaim = "ixo.claims.v1beta1.ClaimUpdatedEvent",
+  evaluateClaim = "ixo.claims.v1beta1.ClaimEvaluatedEvent",
   disputeClaim = "ixo.claims.v1beta1.ClaimDisputedEvent",
+  submitIntent = "ixo.claims.v1beta1.IntentSubmittedEvent",
+  updateIntent = "ixo.claims.v1beta1.IntentUpdatedEvent",
   // token
   createToken = "ixo.token.v1beta1.TokenCreatedEvent",
   updateToken = "ixo.token.v1beta1.TokenUpdatedEvent",
   mintToken = "ixo.token.v1beta1.TokenMintedEvent",
+  transferToken = "ixo.token.v1beta1.TokenTransferredEvent",
+  cancelToken = "ixo.token.v1beta1.TokenCancelledEvent",
+  retireToken = "ixo.token.v1beta1.TokenRetiredEvent",
+  transferCredit = "ixo.token.v1beta1.CreditsTransferredEvent",
   // bonds
   createBond = "ixo.bonds.v1beta1.BondCreatedEvent",
   updateBond = "ixo.bonds.v1beta1.BondUpdatedEvent",
@@ -27,6 +37,22 @@ export enum EventTypes {
   reserveWithdrawalBond = "ixo.bonds.v1beta1.BondWithdrawReserveEvent",
   // Wasm
   wasm = "wasm",
+  // epochs
+  startEpoch = "ixo.epochs.v1beta1.EpochStartEvent",
+  endEpoch = "ixo.epochs.v1beta1.EpochEndEvent",
+  // mint
+  epochProvisionsMinted = "ixo.mint.v1beta1.MintEpochProvisionsMintedEvent",
+  // smartaccount
+  authAdded = "ixo.smartaccount.v1beta1.AuthenticatorAddedEvent",
+  authRemoved = "ixo.smartaccount.v1beta1.AuthenticatorRemovedEvent",
+  authSetActive = "ixo.smartaccount.v1beta1.AuthenticatorSetActiveStateEvent",
+  // liquidstake
+  lsParamsUpdated = "ixo.liquidstake.v1beta1.LiquidStakeParamsUpdatedEvent",
+  lsStake = "ixo.liquidstake.v1beta1.LiquidStakeEvent",
+  lsUpstake = "ixo.liquidstake.v1beta1.LiquidUnstakeEvent",
+  lsAddLSValidator = "ixo.liquidstake.v1beta1.AddLiquidValidatorEvent",
+  lsRebalanced = "ixo.liquidstake.v1beta1.RebalancedLiquidStakeEvent",
+  lsAutoCompound = "ixo.liquidstake.v1beta1.AutocompoundStakingRewardsEvent",
 }
 
 const EventTypesArray = Object.values(EventTypes) as string[];
