55081dc870716a0153a1db71fa5e9249d6011c86
Michael Pretorius <michaelpretorius96@gmail.com>
2024-12-12 14:16:43 +0200

    fix: increase rate limit for liveness probes

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import { getCoreBlock } from "./postgres/query_block";
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minutes
-  max: 10, // Limit each IP to 100 requests per `window`
+  max: 100, // Limit each IP to 100 requests per `window`
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: "Too many requests from this IP, please try again after 1 minutes",
